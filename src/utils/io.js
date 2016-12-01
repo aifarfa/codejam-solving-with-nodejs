c6ef4629d7851e04cd0d889d5242ff891de69306
pasit.r <pasit.rattanathammak@thomsonreuters.com>
2016-12-01 12:21:45 +0700

    refactor and add small input

--- src/utils/io.js
+++ src/utils/io.js
@@ -3,15 +3,13 @@ import {EOL} from 'os'
 
 export const endOfLine = () => EOL
 
-const readAsync = (file) => (resolve, reject) => {
-  fs.readFile(file, (err, data) => {
-    if (err) {
-      reject(err)
-    } else {
-      resolve(data.toString())
-    }
-  })
-}
+const readAsync = (file) => (resolve, reject) => fs.readFile(file, (err, data) => {
+  if (err) {
+    reject(err)
+  } else {
+    resolve(data.toString())
+  }
+})
 
 const writeAsync = (text) => (resolve, reject) => fs.writeFile('output.log', text, (err) => {
   if (err) {
